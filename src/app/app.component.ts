5db7172a5a287e09350d15a69b834d1b4347d14e
Shanmugam <kshanmugam@ad.harman.com>
2020-02-23 12:41:21 +0530

    call-method-imp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Javascript-Concepts';
+  title = 'Javascript-Concepts Call-Apply-Bind';
+
+  constructor() {
+
+    let name ={
+      firstname: 'Kumar',
+      lastname: 'Shan',
+      printFullName: function () {
+        console.log(this.firstname + ' ' + this.lastname);
+      }
+    }
+
+    // Access the method 
+    name.printFullName();
+
+
+    let name2 = {
+      firstname: 'Krishaan',
+      lastname: 'Kumar',
+
+      // This way we can but not good approch
+
+      // printFullName: function () {
+      //   console.log(this.firstname + ' ' + this.lastname);
+      // }
+
+    }
+
+    // Function Borrowing
+    // call method can accept two argumaents FirstArg: scope Of this, SecondArg: Parameters
+    name.printFullName.call(name2);
+
+  }
 }
