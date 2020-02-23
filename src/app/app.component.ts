3884dbe5107d123af3d2431e453672f35a803a5e
Shanmugam <kshanmugam@ad.harman.com>
2020-02-23 16:36:05 +0530

    Polyfill-intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Javascript-Concepts Call-Apply-Bind';
+  title = 'Javascript-Concepts Polyfill for bind method';
 
   constructor() {
 
@@ -14,45 +14,25 @@ export class AppComponent {
     let name ={
       firstname: 'Kumar',
       lastname: 'Shan'
-      // This function Moved to out side of the object
     }
 
-    // common method for use display fullname
-    // here we introduce [hometown argument]
-    let printFullName = function (hometown, state) {
-      console.log(this.firstname + ' ' + this.lastname + ' ' + 'from' + ' ' + hometown + ', ' + state);
+    let printName = function() {
+      console.log(this.firstname + ' ' + this.lastname);
     }
 
+    // Default Bind Method
+    let printMyName = printName.bind(name);
+    printMyName();
 
-    // Object 2
-    let name2 = {
-      firstname: 'Krishaan',
-      lastname: 'Kumar',
-    }
+    
+    // we need to create our own bind() by mybind()
+    // Example Below
+    
+    // mybind() {
+    // }
 
-
-    // Object 3
-    let name3 = {
-      firstname: 'Thillai',
-      lastname: 'Vasuki',
-    }
-
-
-    // Bind Method Start
-    let printMyName = printFullName.bind(name, 'Kumbakonam', 'TamilNadu'); // store the function in let 
-    console.log(printMyName); // Can see the retun function
-    printMyName(); // we can excute the function any time
-
-
-     // Bind Method Start
-     let printMyName2 = printFullName.bind(name2, 'Tirunelveli', 'TamilNadu'); // store the function in let 
-     console.log(printMyName); // Can see the retun function
-     printMyName2(); // we can excute the function any time
-
-     // Bind Method Start
-     let printMyName3 = printFullName.bind(name3, 'Tirunelveli', 'TamilNadu'); // store the function in let 
-     console.log(printMyName); // Can see the retun function
-     printMyName3(); // we can excute the function any time
+    // let printMyName2 = printName.mybind(name);
+    // printMyName2();
 
   }
 }
